fde1d454657d1545d218757d749b7e0229cc867a
Salma_ehab <163453468+Salmon-ehab@users.noreply.github.com>
2025-06-29 23:50:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -38,10 +38,11 @@ migrate = Migrate(app, db)
 
 app.register_blueprint(api_bp, url_prefix='/api')
 
+@app.route('/')
+def home():
+    return "Welcome to the Flask API on Vercel!"
+
 if __name__ == '__main__':
     app.run()
 #host='0.0.0.0', port=5000
 # gunicorn -w 4 -b 0.0.0.0:5000 app:app
-@app.route('/')
-def index():
-    return jsonify({"message": "Welcome to the Flask API on Vercel!"})
